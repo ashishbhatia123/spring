5ac972e9f86a3ef2271d9b9544c0de99f7d5662f
ashishbhatia123 <35377413+ashishbhatia123@users.noreply.github.com>
2019-03-05 17:39:45 +0530

    Add files via upload

---
+++ springMVCApplications2/src/com/cg/springmvc/bean/Customer.java
@@ -0,0 +1,59 @@
+package com.cg.springmvc.bean;
+
+
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String mobileNo;
+	private String email;
+	private String city;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", mobileNo=" + mobileNo
+				+ ", email=" + email + "]";
+	}
+	
+	
+	
+	
+
+}
